c2a9f7cb55942969da8232a317b80a49906c8305
llFireHawkll <sparsh.dtt@gmail.com>
2020-07-31 02:30:28 +0530

    added pyssdml version

--- pySSDML/__init__.py
+++ pySSDML/__init__.py
@@ -4,13 +4,15 @@ Project: pySSDML
 File Created: Thursday, 30th July 2020 7:39:08 pm
 Author: Sparsh Dutta (sparsh.dtt@gmail.com)
 -----
-Last Modified: Friday, 31st July 2020 1:48:02 am
+Last Modified: Friday, 31st July 2020 2:26:15 am
 Modified By: Sparsh Dutta (sparsh.dtt@gmail.com>)
 -----
 Copyright 2020 Sparsh Dutta
 '''
 
 import os
+
+__version__ = '0.0.1'
 
 # Create output folder for saving logs and artifacts
 output_folder_path = os.getcwd().replace("\\",'/')
