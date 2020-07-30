6306dfcaae3266e608fbd61b4f5aed581cf771da
llFireHawkll <sparsh.dtt@gmail.com>
2020-07-31 01:53:05 +0530

    adding config module

---
+++ pySSDML/src/config.py
@@ -0,0 +1,15 @@
+'''
+File: config.py
+Project: src
+File Created: Thursday, 30th July 2020 11:13:07 pm
+Author: Sparsh Dutta (sparsh.dtt@gmail.com)
+-----
+Last Modified: Friday, 31st July 2020 1:50:06 am
+Modified By: Sparsh Dutta (sparsh.dtt@gmail.com>)
+-----
+Copyright 2020 Sparsh Dutta
+'''
+
+import os 
+
+LOGGER_OUTPUT_PATH = str(os.getcwd().replace("\\",'/')) + '/pySSDML_Output/'
